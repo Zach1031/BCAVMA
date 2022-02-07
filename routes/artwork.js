8b67d837d3dbffd50c07677bb245cd4463e52a5e
Zach1031 <zachiscool1031@gmail.com>
2022-02-07 15:01:00 -0500

    Search bar works better (kinda).

--- routes/artwork.js
+++ routes/artwork.js
@@ -4,10 +4,40 @@ var data = require('../dummy_data/data');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('artwork', { title: 'BCAVMA', 
-                        layout: 'layout',
-                        search: 'search',
-                        artwork: data});
+  let id = req.query.id;
+  let search = req.query.search;
+
+  if(!(id == null)){
+    let art = data.find(function (art) { return art.art_id == id });
+
+
+    if (art === undefined) {
+      next(); 
+    }
+    else {
+      res.render('artwork_detail', { title: art.art_title, styles: ["tables", "event"], art: art });
+    }
+  }
+
+  else if(!(search == null)){
+    let art = data.find(function (art) { return art.art_id == search });
+
+
+    if (art === undefined) {
+      next(); 
+    }
+    else {
+      res.render('artwork', { title: 'BCAVMA', layout: 'layout', search: 'search', artwork: {art}});
+    }
+  }
+
+  else {
+    res.render('artwork', { title: 'BCAVMA', 
+    layout: 'layout',
+    search: 'search',
+    artwork: data});
+  }
+  
 });
 
 router.get('/page/:page_number', function(req, res, next) {
@@ -45,12 +75,11 @@ router.get('/page/:page_number', function(req, res, next) {
 router.get('/:art_id', function(req, res, next) {
   let art_id = req.params.art_id;
 
-  // Find matching event in the data (a real database will be easier to query)
   let art = data.find(function (art) { return art.art_id == art_id });
 
 
   if (art === undefined) {
-    next(); //pass along to other handlers (send 404)
+    next(); 
   }
   else {
     res.render('artwork_detail', { title: art.art_title, styles: ["tables", "event"], art: art });
